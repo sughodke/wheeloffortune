b93f99758e22a7b67aa1926752ce399027babaad
Sid <sid.ghodke@gmail.com>
2021-09-06 00:21:15 -0700

    Add function argument to useWorld with a breaking API change

--- src/useWorld.js
+++ src/useWorld.js
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useLayoutEffect} from "react";
+import {useContext, useEffect, useLayoutEffect, useState} from "react";
 import {MatterContext} from "./MatterContext";
 import {Composite} from "matter-js";
 
@@ -10,4 +10,19 @@ export const useWorldAdd = (objs, hack = false) => {
     effectFn(() => {
         Composite.add(engine.world, objs)
     }, [engine])
+}
+
+export const useWorldAdd2 = (fn, hack = false, deps = []) => {
+    // noinspection JSCheckFunctionSignatures
+    const {engine} = useContext(MatterContext)
+    const [objs, setObjs] = useState([])
+
+    const effectFn = hack ? useEffect : useLayoutEffect
+    effectFn(() => {
+        const _objs = fn()
+        Composite.add(engine.world, _objs)
+        setObjs(_objs)
+    }, [engine, ...deps])
+
+    return objs
 }

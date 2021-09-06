5530a0c6dbed535be2e71596d4218df1925c37d5
Sid <sid.ghodke@gmail.com>
2021-09-05 23:46:04 -0700

    Add wheel texture

--- src/Wheel.js
+++ src/Wheel.js
@@ -3,9 +3,48 @@ import {Bodies, Constraint} from "matter-js"
 import {useWorldAdd} from "./useWorld";
 import Needle from "./Needle";
 import Pegs from "./Pegs";
+import useMatter from "./useMatter";
+
+const createTexture = (count = 16, r = 100, cx = 100, cy = 100) => {
+    const canvas = document.createElement('canvas');
+    canvas.width = canvas.height = 200
+    const ctx = canvas.getContext('2d');
+
+    function getRandomColour() {
+        const red = Math.floor(Math.random() * 255);
+        const green = Math.floor(Math.random() * 255);
+        const blue = Math.floor(Math.random() * 255);
+
+        return "rgb(" + red + "," + green + "," + blue + " )";
+    }
+
+    const delta = 2 * Math.PI / count;
+
+    [...Array(count)].forEach((_, i) => {
+        ctx.fillStyle = getRandomColour()
+
+        ctx.beginPath();
+        ctx.moveTo(cx, cy);
+        ctx.arc(cx, cy, r, delta * (i - 1), delta * i);
+        ctx.lineTo(cx, cy);
+        ctx.closePath();
+        ctx.fill();
+    })
+
+    const image = new Image();
+    image.src = canvas.toDataURL();
+
+    return image
+}
 
 // create wheel that can spin
 export default ({ numPegs = 16, outerRadius = 100, onLoad }) => {
+    const { render } = useMatter()
+
+    // Hack to load in the texture
+    useEffect(() => {
+        render.textures['USER_DEFINED_1'] = createTexture(numPegs)
+    }, [render])
 
     const wheelBase = Bodies.circle(0, 0, outerRadius, {
         collisionFilter: {
@@ -14,7 +53,9 @@ export default ({ numPegs = 16, outerRadius = 100, onLoad }) => {
             mask: 0x0
         },
         render: {
-            fillStyle: '#5f5f7e'
+            sprite: {
+                texture: 'USER_DEFINED_1'
+            }
         },
     }, numPegs)
 
